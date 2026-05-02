48bd95210a4ef50e569e97ff3c1fe4273dbf0846
Siraut12321 <gamershiningstar@gmail.com>
2026-05-02 17:10:19 +0500

    fix: serverless db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error('MongoDB connection error:', err.message);
+    throw err;
   }
 };
 
